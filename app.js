03c5e6fffaea6b438c8d5af356f3162f95a5e11e
Nathan Lufuluabo <lufuluabon@outlook.fr>
2024-05-10 19:19:48 +0200

    End of part 1

--- app.js
+++ app.js
@@ -2,18 +2,52 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Request received!");
-  next(); // Pass the request to the next middleware function
-});
+app.use(express.json()); // This middleware is used to parse the body of requests with a JSON payload
 
+// Add headers to the response to avoid CORS errors
 app.use((req, res, next) => {
-  res.status(200); // Set the status code of the response
+  // We don't precise the route, so this middleware will be executed for all requests
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res) => {
-  res.json({ message: "Your request was successful!" });
+app.post("/api/stuff", (req, res, next) => {
+  console.log(req.body); // We can access the body of the request thanks to the body-parser middleware
+  res.status(201).json({
+    message: "Object created!",
+  });
 });
 
+// Route to handle GET requests to the /api/stuff endpoint
+app.get("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
+});
 module.exports = app; // Export the Express app object for use in other modules
